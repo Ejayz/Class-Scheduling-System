5832dcfc949c2bd3ec75776ae5f5c4468638bed1
Aspect <>
2020-06-11 19:12:52 +0800

    Second commit:Added loop to make your calculation continues adding options y to continue to use the program and if you are done your calculation you have an option to do another calculation or exit

--- src/cs_system/DB_class/Setup.java
+++ src/cs_system/DB_class/Setup.java
@@ -17,24 +17,30 @@ import java.util.logging.Logger;
  * @author Administrator
  */
 public class Setup {
-
+//This will be a connection requirment to connect to the database 
+//See ConnectionManager class in BD_class package for method you can use in connection manager
+    
     Connection conn = ConnectionManager.getConnection();
     PreparedStatement stmt;
     ResultSet rs;
-
+//This will update the setup table in mysql if you will accept the term and condition
     public int IsAccept() {
+    //Sql Query to be passed to PrepareStatement Method
         String sql = "update setup set IS_SETUP='true', IS_ACCEPT='true' where ID='1' ";
         int result = 0;
         try {
             stmt = conn.prepareStatement(sql);
+          
             result = stmt.executeUpdate();
         } catch (Exception ex) {
+            //Print exception error
             ex.printStackTrace();
         }
 
         return result;
     }
-
+//This will check if you already done the setup.You dont want to setup files and database always 
+    //This will return 0 if false and 1 if true
     public int checkSetUp() {
         String sql = "select * from setup where IS_ACCEPT='true' AND IS_SETUP='true'";
         int result = 0;
@@ -45,7 +51,7 @@ public class Setup {
                 result = 1;
             }
         } catch (Exception ex) {
-           return 0;
+            return 0;
         }
         return result;
     }
